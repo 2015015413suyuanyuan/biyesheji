4a330f9923e0ecc720542567c7ca4319c021fcb8
“suyuanyuan <suyuanyuan@sharkshopping.com>
2018-11-22 18:01:45 +0800

    111

--- test/src/main.js
+++ test/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css';
+import Tabbar from 'mint-ui';
+
+Vue.use(Mint);
+Vue.component(Tabbar.name, Tabbar);
 
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
--- test/src/router/index.js
+++ test/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Tabbar from '@/components/Tabbar'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/Tabbar',
+      name: 'Tabbar',
+      component: Tabbar
     }
   ]
 })
